9da294911fa1c76cd381ba931373db882d222707
Haley Roe <roehaleyc@gmail.com>
2020-07-17 16:48:44 -0500

    updated versions of diplomacy and unit tests

--- Diplomacy.py
+++ Diplomacy.py
@@ -1,77 +1,122 @@
 # Diplomacy.py
-# contains diplomacy_read(), diplomacy_eval(), diplomacy_print(), and diplomacy_solve()
+# contains diplomacy_eval(), diplomacy_print(), and diplomacy_solve()
+
 from io import StringIO
-# --------------
-# diplomacy_read
-# --------------
-def diplomacy_read(s):
 
-    infile = StringIO(str(s))
-    line = infile.readline()
-    line = line.split()
-    all_armies = []
-    
-    while line != []:
-        all_armies.append(line)
-        line = infile.readline()
-        line = line.split()
-    return all_armies
+# helper functions to kill armies in eval()
+def killArmies(locationDict, location, army_indices):
 
+    # creates variable to find the index of the max value later on
+    maximum = ["", -1]
+    values = locationDict.get(location)
+
+    for val in values:
+        if val[1] > maximum[1]:
+            maximum = [val[0],val[1]]
+
+    # start to kill armies by assigning them a value of -1 if they are dead
+    for val in values:
+        # if current max army has more support, opponent army dies
+        if maximum[1] > val[1]:
+            val[1] = -1
+        # if current max army has the same support as opponent AND they are not the same army, both armies die
+        if (maximum[1] == val[1]) and (maximum[0] != val[0]):
+            val[1] = -1
+            # returns the index of the current max army in the values and kills them too
+            index = [i for i, lst in enumerate(values) if maximum[0] in lst][0]
+            values[index][1] = -1
+    return values
+
+# helper function to change support values of (-1) to "[dead]" in eval()
+def convertDead(locationDict):
+    for location in locationDict.keys():
+        values = locationDict.get(location)
+        for val in values:
+            if val[1] == -1:
+                val[1] = "[dead]"
+    return locationDict
+                
 # --------------
 # diplomacy_eval
 # --------------
 def diplomacy_eval(all_armies):
 
-    # create dictionary of armies and actions
-    army_dict = {}
+    # create dictionary of armies and corresponding indices
+    army_indices = {}
+    index = 0
     for army in all_armies:
-        army_dict[army[0]] = army[1:]
-
+        army_indices[army[0]] = index
+        index += 1
     # execute actions in order: hold, move, support
-    for actions in army_dict.values():
-        if actions[1] == "Hold":
-            del actions[1]
-    for actions in army_dict.values():
-        if actions[1] == "Move":
-            actions[0] = actions[2]
-            del army[1:3]
-    for actions in army_dict.values():
-        if actions[1] == "Support":
-            # fix for list / indexing
-            for army in armies:
-                if len(army) > 2:
-                    supporter = army[1]
-                    in_city = 0
-                    for army_again in armies:
-                        if army[1] == supporter:
-                            in_city += 1
-                    # if army in supporting city --> cancel support
-                    if in_city > 1:
-                        del army_again[2:4]    
-                    # if not army in supporting city --> continue support
-                    else: # if in_city = 1
-                        supported = army_again[3]
-                        
-
+    # hold action
+    for army in all_armies:
+        if army[2] == "Hold":
+            del army[2]
             
-##            #had army_dict[actions[2]]
-##            supported = actions[2]
-##            #if there is already support 
-##            if army_dict.get(supported)[-1].isdigit():
-##                army_dict.get(supported)[-1] += 1
-##            else:
-##                army_dict.get(supported).append(1)
-##                
-##    army_list = []
-##    keylst = army_dict.keys()
-##    for key in keylst:
-##        lst = []
-##        lst.append(key)
-##        for actions in army_dict.get(key):
-##            lst.append(actions)
-        army_list.append(lst)
-                
-    return
+    # move action
+    for army in all_armies:
+        try:
+            if (army[2] == "Move"):
+                army[1] = army[3]
+                del army[2:4]
+        except IndexError:
+            continue
+            
+    # support action
+    for army in all_armies:
+        try:
+            if (army[2] == "Support"):
+                supporter = army[1]
+                in_city = 0
+                for army_again in all_armies:
+                    if army_again[1] == supporter:
+                        in_city += 1
+
+                # if army in supporting city --> cancel support
+                if in_city > 1:
+                    del army[2:4]    
+                # if not army in supporting city --> continue support
+                else: # if in_city = 1
+                    supported = army[3] # supported = actions[2]
+                    index = army_indices.get(supported)
+                    # if there is already support 
+                    if (all_armies[index][-1]).isdigit(): 
+                        (all_armies[index][-1]) += 1
+                        del army[2:4]
+                    # if no prior support
+                    else:
+                        all_armies[index].append(1)
+                        del army[2:4]
+        except IndexError:
+            continue
+
+    # WAR
+
+    # group armies by location
+    locationDict = {}
+    for army in all_armies:
+        if army[1] not in locationDict:
+            if len(army) > 2:
+                locationDict[str(army[1])] = [[army[0], army[2]]]
+            else:
+                locationDict[army[1]] = [[army[0], 0]]
+        else: # if in locationDict
+            val = locationDict.get(army[1])
+            if len(army) > 2:
+                val.append([army[0], army[2]])
+            else:
+                val.append([army[0], 0])
+            locationDict[army[1]] = val
+
+    # kill appropriate armies at each location
+    for location in locationDict.keys():
+        after_war = killArmies(locationDict, location, army_indices)
+        locationDict[location] = after_war
+
+    # convert -1 in locationDict to "[dead]"
+    after_war_dict = convertDead(locationDict)
+
+    return after_war_dict
 
 # ---------------
 # diplomacy_print
@@ -80,34 +125,46 @@ def diplomacy_print(w, result):
     """
     print one armies outcome
     w - a writer
-    result - one armies result (either [dead] or their location)
+    result - a sorted dictionary of locations and the armies results (either [dead] or their support)
     """
-    for i in result:
-        if result[i] == result[len(result) - 1]:
-            w.write("\n")
-        else:
-            w.write(i + " ")       
+
+    result_lst = []
+    for key in result.keys():
+        try:
+            values = result.get(key)
+            for val in values:
+                if val[1] == "[dead]":
+                    result_lst.append(val[0] + " " + val[1])
+                else:
+                    result_lst.append(val[0] + " " + key)
+        except IndexError:
+            result_lst.append(val[0] + " " + key)
+
+    for i in result_lst:
+        w.write(i + "\n")
 
 # ---------------
 # diplomacy_solve
 # ---------------
 def diplomacy_solve(r, w):
-
-    # pre-condition / argument validity
-    assert r != ""
-    
     """
     r a reader
     w a writer
     """
-    all_armies = diplomacy_read(r)
+
+    # pre-condition / argument validity
+    assert r != ""
+
+    all_armies = []
+    for s in r:
+        line = s.split()
+        all_armies.append(line)        
+    
+    #all_armies = diplomacy_read(r)
     after_war = diplomacy_eval(all_armies)
 
     # post-condition / return-value validity
-    assert after_war != []
-    
-    for result in after_war:
-        diplomacy_print(w, result)
-
+    assert after_war != {}
+    diplomacy_print(w, after_war)
 
 
--- TestDiplomacy.py
+++ TestDiplomacy.py
@@ -1,57 +1,33 @@
 # TestDiplomacy.py
 # unit tests that test corner and failure cases for diplomacy_solve()
 
+from io import StringIO
 from unittest import main, TestCase
 
-from Diplomacy import diplomacy_read, diplomacy_solve
+from Diplomacy import diplomacy_solve, diplomacy_eval, diplomacy_print
 
 # -------------
 # TestDiplomacy
 # -------------
 
 
-class TestCollatz (TestCase):
-
-    # ----
-    # read
-    # ----
-
-    def test_read(self):
-        s = "A Madrid Hold\nB Barcelona Move Madrid\nC London Move Madrid\nD Paris Support B\nE Austin Support A\n"
-        i = diplomacy_read(s)
-        self.assertEqual(i,  [["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Move", "Madrid"],\n
-                              ["D", "Paris", "Support", "B"], ["E", "Austin", "Support", "A"]])
-
-    def test_read_2(self):
-        s = "A Madrid Hold\nB Barcelona Move Madrid\nC London Support B\nD Austin Move London\n"
-        i = diplomacy_read(s)
-        self.assertEqual(i,  [["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Support", "B"],\n
-                              ["D", "Austin", "Move", "London"]])
-
-    def test_read_3(self):
-        s = "A Madrid Hold\nB Barcelona Move Madrid\nC London Move Madrid\nD Paris Support B\n"
-        i = diplomacy_read(s)
-        self.assertEqual(i,  [["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Move", "Madrid"],\n
-                              ["D", "Paris", "Support", "B"]])
+class TestDiplomacy (TestCase):
 
     # ----
     # eval
     # ----
 
     def test_eval(self):
-        v = diplomacy_eval([["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Move", "Madrid"],\n
-                              ["D", "Paris", "Support", "B"], ["E", "Austin", "Support", "A"]])
-        self.assertEqual(v, [["A", "[dead]"], ["B", "[dead]"], ["C", "[dead]"], ["D", "Paris"], ["E", "Austin"]])
+        v = diplomacy_eval([["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Move", "Madrid"], ["D", "Paris", "Support", "B"], ["E", "Austin", "Support", "A"]])
+        self.assertEqual(v, {"Madrid":[["A",1], ["B",1], ["C",0]], "Paris":[["D",0]], "Austin":[["E",0]]})
 
     def test_eval_2(self):
-        v = diplomacy_eval([["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Support", "B"],\n
-                              ["D", "Austin", "Move", "London"]])
-        self.assertEqual(v, [["A", "[dead]"], ["B", "[dead]"], ["C", "[dead]"], ["D", "[dead]"]])
+        v = diplomacy_eval([["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Support", "B"], ["D", "Austin", "Move", "London"]])
+        self.assertEqual(v, {"Madrid":[["A",0],["B",0]], "London":[["C",0], ["D",0]]})
 
     def test_eval_3(self):
-        v = diplomacy_solve([["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Move", "Madrid"],\n
-                              ["D", "Paris", "Support", "B"]])
-        self.assertEqual(v, [["A", "[dead]"], ["B", "Madrid"], ["C", "[dead]"], ["D", "Paris"]])
+        v = diplomacy_eval([["A", "Madrid", "Hold"], ["B", "Barcelona", "Move", "Madrid"], ["C", "London", "Move", "Madrid"], ["D", "Paris", "Support", "B"]])
+        self.assertEqual(v, {"Madrid":[["A",0],["B",1],["C",0]],"Paris":[["D",0]]})
 
     # -----
     # print
@@ -59,18 +35,18 @@ class TestCollatz (TestCase):
 
     def test_print(self):
         w = StringIO()
-        diplomacy_print(w, ["A", "Madrid"])
-        self.assertEqual(w.getvalue(), "A Madrid\n")
+        diplomacy_print(w, {"Madrid":[["A",1], ["B",1], ["C",0]], "Paris":[["D",0]], "Austin":[["E",0]]})
+        self.assertEqual(w.getvalue(), "A [dead]\nB [dead]\nC [dead]\nD Paris\nE Austin\n")
 
     def test_print_2(self):
         w = StringIO()
-        diplomacy_print(w, ["B", "[dead]"])
-        self.assertEqual(w.getvalue(), "B [dead]\n")
+        diplomacy_print(w, {"Madrid":[["A",0],["B",0]], "London":[["C",0], ["D",0]]})
+        self.assertEqual(w.getvalue(), "A [dead]\nB [dead]\nC [dead]\nD [dead]\n")
 
     def test_print_3(self):
         w = StringIO()
-        diplomacy_print(w, ["D", "Paris"])
-        self.assertEqual(w.getvalue(), "D Paris\n")
+        diplomacy_print(w, {"Madrid":[["A",0],["B",1],["C",0]],"Paris":[["D",0]]})
+        self.assertEqual(w.getvalue(), "A [dead]\nB Madrid\nC [dead]\nD Paris\n")
 
     # -----
     # solve
